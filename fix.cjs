9d67a5582df4ae11c325b551d805e3030c5ec032
LuisCFunes <luiscfunes521@gmail.com>
2026-08-30 14:52:46 -0600

    Cleanup remaining files and format fixes

--- fix.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/scripts/dashboard.ts', 'utf8');
-code = code.replace(/\\n/g, '\n');
-fs.writeFileSync('src/scripts/dashboard.ts', code);
-console.log('Fixed newlines.');
